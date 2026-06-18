9609f5f08754ea72962765c797a2e4896490bc3f
TomasBurgos <149694007+Radioactive6363@users.noreply.github.com>
2026-06-18 02:15:07 -0300

    Update GameManager.cs

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -76,9 +76,18 @@ public class GameManager : MonoBehaviourPunCallbacks
             PhotonNetwork.LoadLevel(duelScenes[0]); 
         }
     }
-    
-    public override void OnEnable() { SceneManager.sceneLoaded += OnSceneLoaded; }
-    public override void OnDisable() { SceneManager.sceneLoaded -= OnSceneLoaded; }
+
+    public override void OnEnable()
+    {
+        base.OnEnable();
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    public override void OnDisable()
+    {
+        base.OnDisable();
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
 
     private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
